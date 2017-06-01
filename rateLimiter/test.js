fbb6bcce4b6d0954b98289a885d3515314679124
Taku <gcobs135102@gmail.com>
2017-06-02 03:04:19 +0800

    test behavior of rate limiter

--- rateLimiter/test.js
+++ rateLimiter/test.js
@@ -5,7 +5,7 @@ const rateLimiter = require('./index.js');
 const { expect } = chai;
 const app = express();
 const REQUEST_LIMIT = 4;
-const RESET_TIME = 2000;
+const RESET_TIME = 3000;
 
 app.set('port', process.env.PORT || 3000);
 
@@ -18,23 +18,53 @@ app.get('/', (req, res) => {
 });
 chai.use(chaiHttp);
 
-describe('rate limiter', function () {
-	describe(`request ${REQUEST_LIMIT + 1} times`, function () {
-		for (let i = 1; i <= REQUEST_LIMIT + 1; i++) {
-			it(`${i} times`, function (done) {
+//	test
+
+describe(`send request w/ request limit: ${REQUEST_LIMIT}`, function () {
+	describe(`request ${REQUEST_LIMIT + 2} times`, function () {
+		for (let i = 1; i <= REQUEST_LIMIT + 2; i++) {
+			it(`res #${i} should have proper status and header`, function (done) {
 				chai.request(app)
 					.get('/')
 					.end(function (err, res) {
-						if (i == REQUEST_LIMIT + 1) {
+						if (i > REQUEST_LIMIT) {
 							expect(res).to.have.status(429);
+							expect(res).to.have.header('X-RateLimit-Remaining', '0');
 						}
 						else {
 							expect(err).to.be.null;
 							expect(res).to.not.have.status(429);
+							expect(res).to.have.header('X-RateLimit-Remaining', (REQUEST_LIMIT - i).toString());
 						}
 						done();
 					});
 			});
 		}
+	});
+	describe('reset limit', function () {
+		this.timeout(RESET_TIME + 3000);
+		it('be going to reset', function (done) {
+			setTimeout(function () {
+				chai.request(app)
+					.get('/')
+					.end(function (err, res) {
+						expect(res).to.have.status(429);
+						expect(res).to.have.header('X-RateLimit-Remaining', '0');
+						done();
+					});
+			}, RESET_TIME - 1000);
+		});
+		it('limit should reset', function (done) {
+			setTimeout(function () {
+				chai.request(app)
+					.get('/')
+					.end(function (err, res) {
+						expect(err).to.be.null;
+						expect(res).to.not.have.status(429);
+						expect(res).to.have.header('X-RateLimit-Remaining', (REQUEST_LIMIT - 1).toString());
+						done();
+					});
+			}, 1000);
+		});
 	});
 });
