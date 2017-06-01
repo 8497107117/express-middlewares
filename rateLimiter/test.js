af1f9f75c95840265c5fe522cfeef5cda37b5f4b
Taku <gcobs135102@gmail.com>
2017-06-01 21:58:32 +0800

    implement & coverage

--- rateLimiter/test.js
+++ rateLimiter/test.js
@@ -4,7 +4,7 @@ const express = require('express');
 const rateLimiter = require('./index.js');
 const { expect } = chai;
 const app = express();
-const REQUEST_LIMIT = 10;
+const REQUEST_LIMIT = 4;
 const RESET_TIME = 2000;
 
 app.set('port', process.env.PORT || 3000);
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000);
 //  Disable X-Powered-By Header
 app.disable('x-powered-by');
 app.listen(app.get('port'));
-app.use(rateLimiter());
+app.use(rateLimiter({ requestLimit: REQUEST_LIMIT, resetTime: RESET_TIME }));
 app.get('/', (req, res) => {
 	res.send('GETTTTTT');
 });
@@ -25,11 +25,11 @@ describe('rate limiter', function () {
 				chai.request(app)
 					.get('/')
 					.end(function (err, res) {
-						expect(err).to.be.null;
 						if (i == REQUEST_LIMIT + 1) {
 							expect(res).to.have.status(429);
 						}
 						else {
+							expect(err).to.be.null;
 							expect(res).to.not.have.status(429);
 						}
 						done();
